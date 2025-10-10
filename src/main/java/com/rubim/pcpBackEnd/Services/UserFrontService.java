fddc9f80b8f2c564ab7301fd7f72d46fad00411e
FelipeCherry3 <felipeelias609@hotmail.com>
2025-10-10 15:10:54 -0300

    ajuste service

--- src/main/java/com/rubim/pcpBackEnd/Services/UserFrontService.java
+++ src/main/java/com/rubim/pcpBackEnd/Services/UserFrontService.java
@@ -3,10 +3,12 @@ package com.rubim.pcpBackEnd.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.rubim.pcpBackEnd.Entity.UserFrontEntity;
 import com.rubim.pcpBackEnd.repository.UserFrontRepository;
 
+@Service
 public class UserFrontService {
 
     @Autowired
